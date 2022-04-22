b5972292559678fbd62e63b23ed7d5cecaa53e07
Harry Brown <harry.brown@checkout.com>
2022-04-22 16:01:29 +0100

    Update versions to 3.5.3

--- Source/Core/Constants.swift
+++ Source/Core/Constants.swift
@@ -1,7 +1,7 @@
 struct Constants {
     
     static let productName = "frames-ios-sdk"
-    static let version = "3.5.2"
+    static let version = "3.5.3"
     static let userAgent = "checkout-sdk-frames-ios/\(version)"
 
     struct Logging {
