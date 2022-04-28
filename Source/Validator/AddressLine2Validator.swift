42a7f4ebfd3e6be2682d6703337540e24cf9e256
Ehab Al <ehab.alsharkawy@checkout.com>
2022-04-28 22:57:51 +0100

    Clean validators code

--- Source/Validator/AddressLine2Validator.swift
+++ Source/Validator/AddressLine2Validator.swift
@@ -2,12 +2,11 @@ import Foundation
 
 class AddressLine2Validator: Validator {
     func validate(text: String?) -> Bool {
-        return isEmpty(text: text)
+        isEmpty(text: text)
     }
     
     private func isEmpty(text: String?) -> Bool {
-        guard text?.isEmpty != false else { return true }
-        return false
+        text?.isEmpty ?? false
     }
 
 }
--- Source/Validator/PhoneNumberValidator.swift
+++ Source/Validator/PhoneNumberValidator.swift
@@ -2,12 +2,11 @@ import Foundation
 
 class PhoneNumberValidator: Validator {
     func validate(text: String?) -> Bool {
-        return isEmpty(text: text)
+        isEmpty(text: text)
     }
     
     private func isEmpty(text: String?) -> Bool {
-        guard text?.isEmpty != false else { return true }
-        return false
+        text?.isEmpty ?? false
     }
 
 }
