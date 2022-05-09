d554c26456e563c6f07a1e9203420bdbc245f69c
Ehab Al <ehab.alsharkawy@checkout.com>
2022-05-09 15:03:11 +0100

    Support Pods

--- iOS Example Frame/iOS Example Frame/MainViewController.swift
+++ iOS Example Frame/iOS Example Frame/MainViewController.swift
@@ -9,101 +9,50 @@
 import UIKit
 import Frames
 
-class MainViewController: UIViewController, CardViewControllerDelegate, ThreedsWebViewControllerDelegate {
-
-    @IBOutlet weak var goToPaymentPageButton: UIButton!
-    @IBOutlet weak var createTokenWithApplePay: UIButton!
-    @IBOutlet weak var threeDSURLTextField: UITextField!
-
-    private static let successURL = URL(string: "https://httpstat.us/200")!
-    private static let failureURL = URL(string: "https://httpstat.us/403")!
+class MainViewController: UIViewController, CardViewControllerDelegate {
     
-    // Step1 : create instance of CheckoutAPIClient
-    let checkoutAPIClient = CheckoutAPIClient(publicKey: "pk_test_6e40a700-d563-43cd-89d0-f9bb17d35e73",
-                                              environment: .sandbox)
-
+    @IBOutlet weak var goToPaymentPageButton: UIButton!
+    
     @IBAction func goToPaymentPage(_ sender: Any) {
+        cardViewController.isNewUI = false
         navigationController?.pushViewController(cardViewController, animated: true)
     }
-
-
+    
+    @IBAction func goToNewPaymentPage(_ sender: Any) {
+        cardViewController.isNewUI = true
+        navigationController?.pushViewController(cardViewController, animated: true)
+    }
+    
+    
     func onSubmit(controller: CardViewController) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
     }
 
     lazy var cardViewController: CardViewController = {
-        let b = CardViewController(checkoutApiClient: checkoutAPIClient, cardHolderNameState: .normal, billingDetailsState: .required, defaultRegionCode: "GB")
-        b.billingDetailsAddress = CkoAddress(addressLine1: "Test line1", addressLine2: "Test line2", city: "London", state: "London", zip: "N12345", country: "GB")
-        b.billingDetailsPhone = CkoPhoneNumber(countryCode: "44", number: "77 1234 1234")
-        b.delegate = self
-        b.addressViewController.setFields(address: b.billingDetailsAddress!, phone: b.billingDetailsPhone!)
-        return b
+        let checkoutAPIClient = CheckoutAPIClient(publicKey: "pk_test_6e40a700-d563-43cd-89d0-f9bb17d35e73",
+                                                  environment: .sandbox)
+        let view = CardViewController(checkoutApiClient: checkoutAPIClient, cardHolderNameState: .normal, billingDetailsState: .normal, defaultRegionCode: "GB")
+        view.billingDetailsAddress = CkoAddress(addressLine1: "Test line1", addressLine2: "Test line2", city: "London", state: "London", zip: "N12345", country: "GB")
+        view.billingDetailsPhone = CkoPhoneNumber(countryCode: "44", number: "77 1234 1234")
+        view.delegate = self
+        view.addressViewController.setFields(address: view.billingDetailsAddress!, phone: view.billingDetailsPhone!)
+        view.delegate = self
+        view.rightBarButtonItem = UIBarButtonItem(title: "Pay", style: .done, target: nil, action: nil)
+        view.availableSchemes = [.visa, .mastercard, .maestro]
+        view.setDefault(regionCode: "GB")
+        view.addressViewController.setCountrySelected(country: "GB", regionCode: "GB")
+        return view
     }()
 
     @IBAction func onClickGoToPaymentPage(_ sender: Any) {
         navigationController?.pushViewController(cardViewController, animated: true)
     }
 
-    @IBAction func onClickGoTokenWithApplePay(_ sender: Any) {
-        
-        // Use example Apple Pay payment data.
-        guard let paymentDataURL = Bundle.main.url(
-                forResource: "example_apple_pay_payment_data",
-                withExtension: "json") else {
-            print("Unable to get URL of Apple Pay payment data.")
-            return
-        }
-
-        let paymentData: Data
-        do {
-            paymentData = try Data(contentsOf: paymentDataURL)
-        } catch {
-            print(error.localizedDescription)
-            return
-        }
-
-        checkoutAPIClient.createApplePayToken(paymentData: paymentData) { status in
-            switch status {
-            case .failure(let error):
-                self.showAlert(with: error.localizedDescription)
-            case .success(let CkoCardTokenResponse):
-                self.showAlert(with: CkoCardTokenResponse.token)
-            }
-        }
-
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        cardViewController.delegate = self
-        cardViewController.rightBarButtonItem = UIBarButtonItem(title: "Pay", style: .done, target: nil, action: nil)
-        cardViewController.availableSchemes = [.visa, .mastercard, .maestro]
-        cardViewController.setDefault(regionCode: "GB")
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        cardViewController.addressViewController.setCountrySelected(country: "GB", regionCode: "GB")
-    }
-
     func onTapDone(controller: CardViewController, cardToken: CkoCardTokenResponse?, status: CheckoutTokenStatus) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
         switch status {
         case .success:
-
-            // **** For testing only. ****
-            print("addressLine1 : \(cardToken?.billingAddress?.addressLine1 ?? "")")
-            print("addressLine2 : \(cardToken?.billingAddress?.addressLine2 ?? "")")
-            print("countryCode \(cardToken?.phone?.countryCode ?? "")")
-            print("phone number \(cardToken?.phone?.number ?? "")")
-            // **** For testing only. ****
-
-            guard let cardToken = cardToken else {
-                self.showAlert(with: "Token object is nil")
-                return
-            }
-            self.showAlert(with: cardToken.token)
-
+            self.showAlert(with: cardToken!.token)
         case .failure:
             print("failure")
         }
@@ -117,28 +66,5 @@ class MainViewController: UIViewController, CardViewControllerDelegate, ThreedsW
         }
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
-    }
-
-
-    @IBAction func onStart3DS(_ sender: Any) {
-        guard let threeDSURLString = threeDSURLTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-              let threeDSURL = URL(string: threeDSURLString) else {
-            showAlert(with: "3DS URL could not be parsed")
-            return
-        }
-
-        let threedsWebViewController = ThreedsWebViewController(successUrl: Self.successURL, failUrl: Self.failureURL)
-        threedsWebViewController.delegate = self
-        threedsWebViewController.url = threeDSURL.absoluteString
-
-        present(threedsWebViewController, animated: true, completion: nil)
-    }
-
-    func threeDSWebViewControllerAuthenticationDidSucceed(_ threeDSWebViewController: ThreedsWebViewController, token: String?) {
-        showAlert(with: "3DS success, token: \(token ?? "nil")")
-    }
-
-    func threeDSWebViewControllerAuthenticationDidFail(_ threeDSWebViewController: ThreedsWebViewController) {
-        showAlert(with: "3DS Fail")
     }
 }
