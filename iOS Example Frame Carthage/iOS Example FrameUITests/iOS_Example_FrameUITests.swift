a59ea3a147f01c7fea0ce03b65e50228e382730f
Ehab Al <ehab.alsharkawy@checkout.com>
2022-05-16 13:16:10 +0100

    Fix Carthage project confilct

--- iOS Example Frame Carthage/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
+++ iOS Example Frame Carthage/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
@@ -33,7 +33,7 @@ class iOS_Example_FrameUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         let app = XCUIApplication()
-        app.buttons["OLD"].tap()
+        app.buttons["Go to payment page"].tap()
         let elementsQuery = app.scrollViews.otherElements
         elementsQuery.textFields["4242"].tap()
         elementsQuery.textFields["4242"].typeText("424242424242424242")
