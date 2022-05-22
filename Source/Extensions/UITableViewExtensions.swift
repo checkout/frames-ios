ea32f55cdfea9cf6c128f0d014026aacdec59819
Ehab Al <ehab.alsharkawy@checkout.com>
2022-05-22 20:57:37 +0100

    Add reusable UITableView `register` and `dequeue `cell

--- Source/Extensions/UITableViewExtensions.swift
+++ Source/Extensions/UITableViewExtensions.swift
@@ -1,10 +1,36 @@
 import UIKit
+
+protocol Reusable: AnyObject{
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    static var reuseIdentifier: String {
+        String(describing: self)
+    }
+}
+
+extension UITableViewCell: Reusable{}
+
+extension UITableView {
+
+    func register<T: UITableViewCell>(_: T.Type)  {
+        register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+
+    func dequeueReusable<T: UITableViewCell>(for indexPath: IndexPath) -> T  {
+        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        return cell
+    }
+}
+
 extension UITableView {
 
     func setBottomInset(to value: CGFloat) {
         let edgeInset = UIEdgeInsets(top: 0, left: 0, bottom: value, right: 0)
-        self.contentInset = edgeInset
-        self.scrollIndicatorInsets = edgeInset
-
+        contentInset = edgeInset
+        scrollIndicatorInsets = edgeInset
     }
 }
