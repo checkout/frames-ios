ff4f1e52070e7b92711e4d21ac10e2148b701fd1
Harry Brown <harry.brown@checkout.com>
2022-06-21 16:25:10 +0100

    PIMOB-1270: fix country tests

--- Checkout/Checkout/Test/Model/CountryTests.swift
+++ Checkout/Checkout/Test/Model/CountryTests.swift
@@ -245,7 +245,7 @@ final class CountryTests: XCTestCase {
       Country(iso3166Alpha2: "AE", dialingCode: "971"),
       Country(iso3166Alpha2: "GB", dialingCode: "44"),
       Country(iso3166Alpha2: "US", dialingCode: "1"),
-      Country(iso3166Alpha2: "UMI", dialingCode: "246"),
+      Country(iso3166Alpha2: "UM", dialingCode: "246"),
       Country(iso3166Alpha2: "UY", dialingCode: "598"),
       Country(iso3166Alpha2: "UZ", dialingCode: "998"),
       Country(iso3166Alpha2: "VU", dialingCode: "678"),
