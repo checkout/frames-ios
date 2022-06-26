6145053768f4b74513aa081476f4423715efd500
Ehab Al <ehab.alsharkawy@checkout.com>
2022-06-26 21:27:16 +0100

    Update Carthage files

--- iOS Example Frame/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
+++ iOS Example Frame/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
@@ -33,7 +33,7 @@ class iOS_Example_FrameUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         let app = XCUIApplication()
-        app.buttons["Default UI"].tap()
+        app.buttons["Old"].tap()
         let elementsQuery = app.scrollViews.otherElements
         elementsQuery.textFields["4242"].tap()
         elementsQuery.textFields["4242"].typeText("424242424242424242")
