66c486c8b008ab713c97ede0af832fab7086b5ed
Ehab Al <ehab.alsharkawy@checkout.com>
2022-06-27 14:08:15 +0100

    Fix Carthage UI Test

--- iOS Example Frame/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
+++ iOS Example Frame/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
@@ -43,7 +43,7 @@ class iOS_Example_FrameUITests: XCTestCase {
 
         elementsQuery.textFields["100"].tap()
         elementsQuery.textFields["100"].typeText("100")
-        app.scrollViews.otherElements.staticTexts["Billing Details*"].tap()
+        elementsQuery.staticTexts["Billing Details*"].tap()
         app.navigationBars["Billing"].buttons["Done"].tap()
         app.toolbars["Toolbar"].buttons["Done"].tap()
         app.navigationBars["Payment"].buttons["Pay"].tap()
