ad275db15534bc58023b7b54add2cf34ece7bbf5
Ehab Al <ehab.alsharkawy@checkout.com>
2022-06-29 17:12:45 +0100

    Support textfield as SecuredView

---
+++ Source/UI/NewUI/CommonUI/View/Core/SecureDisplayView.swift
@@ -0,0 +1,98 @@
+//
+//  SecureDisplayView.swift
+//  Frames
+//
+//  Created by Alex Ioja-Yang on 
+//  Copyright © 2022 Checkout. All rights reserved.
+//
+
+import UIKit
+
+/**
+Secure display view preventing leaking of content
+ */
+public final class SecureDisplayView: UIView {
+
+    // MARK: Protect content from leaking
+    public override var subviews: [UIView] { [] }
+    public override var inputView: UIView? { nil }
+    public override var inputViewController: UIInputViewController? { nil }
+    public override var textInputMode: UITextInputMode? { nil }
+    public override var isUserInteractionEnabled: Bool {
+        get {
+            guard let secured = super.subviews.first,
+                  secured.isUserInteractionEnabled,
+                  secured.canBecomeFirstResponder else {
+                return false
+            }
+            secured.becomeFirstResponder()
+            return true
+        }
+        set {
+            super.isUserInteractionEnabled = newValue
+        }
+    }
+    public override var accessibilityLabel: String? {
+        get { nil }
+        set { }
+    }
+    public override var accessibilityValue: String? {
+        get { nil }
+        set { }
+    }
+    public override var accessibilityHint: String? {
+        get { nil }
+        set { }
+    }
+    public override var isAccessibilityElement: Bool {
+        get { false }
+        set { }
+    }
+
+    // MARK: Protect methods that may be used to trick view into leaking content
+    public override func reloadInputViews() { }
+    public override func insertSubview(_ view: UIView, at index: Int) { }
+    public override func exchangeSubview(at index1: Int, withSubviewAt index2: Int) { }
+    public override func addSubview(_ view: UIView) { }
+    public override func insertSubview(_ view: UIView, belowSubview siblingSubview: UIView) { }
+    public override func insertSubview(_ view: UIView, aboveSubview siblingSubview: UIView) { }
+    public override func bringSubviewToFront(_ view: UIView) { }
+    public override func sendSubviewToBack(_ view: UIView) { }
+    public override func didAddSubview(_ subview: UIView) { }
+    public override func willRemoveSubview(_ subview: UIView) { }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        super.subviews.first?.frame = bounds
+    }
+
+    public override func systemLayoutSizeFitting(_ targetSize: CGSize) -> CGSize {
+        super.subviews.first?.systemLayoutSizeFitting(targetSize) ?? .zero
+    }
+
+    public override func systemLayoutSizeFitting(_ targetSize: CGSize,
+                                                 withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority,
+                                                 verticalFittingPriority: UILayoutPriority) -> CGSize {
+        super.subviews.first?.systemLayoutSizeFitting(targetSize,
+                                                      withHorizontalFittingPriority: horizontalFittingPriority,
+                                                      verticalFittingPriority: verticalFittingPriority) ?? .zero
+    }
+
+    // MARK: Protect views
+
+    /// Create SecureDisplayView protecting the provided view.
+    public convenience init(secure subview: UIView) {
+        self.init(frame: .zero)
+
+        backgroundColor = .clear
+        super.addSubview(subview)
+    }
+
+    /**
+     Will protect the provided view.
+     */
+    public static func secure(_ view: UIView) -> SecureDisplayView {
+        SecureDisplayView(secure: view)
+    }
+}
