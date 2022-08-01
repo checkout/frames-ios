a3260aebe015d8d751f392c05dcbacfa5407d244
Alex Ioja-Yang <alex.ioja-yang@checkout.com>
2022-08-01 11:39:38 +0100

    fix flaky test not accounting for time zone offset

--- Checkout/Checkout/Integration/CardValidatorTests.swift
+++ Checkout/Checkout/Integration/CardValidatorTests.swift
@@ -62,7 +62,8 @@ final class CardValidatorTests: XCTestCase {
   }
 
   func test_expiryDate_previousMonth() {
-    let currentDate = Date()
+    let utc12SecondOffset: Double = 60 * 60 * 12
+    let currentDate = Date(timeInterval: -utc12SecondOffset, since: Date())
     guard let previousMonth = calendar.date(byAdding: .month, value: calendar.component(.day, from: currentDate) > 15 ? -2 : -1, to: currentDate) else {
       XCTFail("could not build next month date")
       return
