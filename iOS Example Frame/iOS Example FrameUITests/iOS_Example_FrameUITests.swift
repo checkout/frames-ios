f68b16bd0c68b148e815e2832e1297de8cf3d429
Ehab Al <ehab.alsharkawy@checkout.com>
2022-08-18 16:07:21 +0100

    Add UI test target for Pod project

---
+++ iOS Example Frame/iOS Example FrameUITests/iOS_Example_FrameUITests.swift
@@ -0,0 +1,11 @@
+//
+//  iOS_Example_FrameUITests.swift
+//  iOS Example FrameUITests
+//
+//  Created by Ehab Alsharkawy.
+//  Copyright © 2022 Checkout. All rights reserved.
+//
+
+import XCTest
+
+class iOS_Example_FrameUITests: XCTestCase { }
