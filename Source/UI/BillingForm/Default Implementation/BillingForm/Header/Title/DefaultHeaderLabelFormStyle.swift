6acb2998c270a88cc3197023f95e5696197af8a7
Ehab Al <ehab.alsharkawy@checkout.com>
2022-09-14 11:23:30 +0100

    Title bar: Change styles to match Title 2

--- Source/UI/BillingForm/Default Implementation/BillingForm/Header/Title/DefaultHeaderLabelFormStyle.swift
+++ Source/UI/BillingForm/Default Implementation/BillingForm/Header/Title/DefaultHeaderLabelFormStyle.swift
@@ -4,7 +4,7 @@ public struct DefaultHeaderLabelFormStyle: ElementStyle {
     public var textAlignment: NSTextAlignment = .natural
     public var backgroundColor: UIColor = .clear
     public var isHidden = false
-    public var text: String = Constants.LocalizationKeys.BillingForm.Header.title
-    public var font = UIFont.systemFont(ofSize: Constants.Style.BillingForm.HeaderTitle.fontSize.rawValue)
-    public var textColor: UIColor  = .codGray
+    public var text: String = ""
+    public var font = CKOUIStyle.Font.Title.title2
+    public var textColor: UIColor = CKOUIStyle.Color.Text.primary
 }
