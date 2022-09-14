0e41c861d706eb80ef1a80c418aafad04d0e0eb2
Ehab Al <ehab.alsharkawy@checkout.com>
2022-09-14 16:51:02 +0100

    Update UI style classes and unit tests

--- Source/UI/CommonUI/UIStyle/CKOUIStyle+UIFont.swift
+++ Source/UI/CommonUI/UIStyle/CKOUIStyle+UIFont.swift
@@ -1,37 +1,65 @@
 import UIKit
 
-enum CKOUIStyle {}
+/// Default Checkout UI Style
+public enum CKOUIStyle {}
 
-extension CKOUIStyle {
+public extension CKOUIStyle {
 
+    /// Custom Font for Checkout  UI style
     enum Font {
-        enum Title {
-            static let title2 = CKOUIStyle.font(size: 24, weight: .medium)
-            static let headline = CKOUIStyle.font(size: 17, weight: .semibold)
-        }
 
-        enum Body {
-            static let large = CKOUIStyle.font(size: 17, weight: .regular)
-            static let `default` = CKOUIStyle.font(size: 15, weight: .medium)
-            static let defaultPlus = CKOUIStyle.font(size: 15, weight: .regular)
-            static let small = CKOUIStyle.font(size: 13, weight: .medium)
-            static let smallPlus = CKOUIStyle.font(size: 13, weight: .regular)
-        }
+        // MARK: - Title -
 
-        enum Input {
-            static let label = CKOUIStyle.font(size: 15, weight: .regular)
-        }
+        /// A font for big title with size 24 and medium weight
+        static let title2 = CKOUIStyle.font(size: 24, weight: .medium)
 
-        enum Action {
-            static let large = CKOUIStyle.font(size: 15, weight: .medium)
-            static let `default` = CKOUIStyle.font(size: 15, weight: .regular)
-        }
+        /// A font for headline with size 17 and `semibold` weight
+        static let headline = CKOUIStyle.font(size: 17, weight: .semibold)
+
+        // MARK: - Body -
+
+        /// A font for large body with size 17 and regular weight
+        static let bodyLarge = CKOUIStyle.font(size: 17, weight: .regular)
+
+        /// A font for default body with size 15 and medium weight
+        static let bodyDefault = CKOUIStyle.font(size: 15, weight: .medium)
+
+        /// A font for default plus body with size 15 and regular weight
+        static let bodyDefaultPlus = CKOUIStyle.font(size: 15, weight: .regular)
+
+        /// A font for small body with size 13 and medium weight
+        static let bodySmall = CKOUIStyle.font(size: 13, weight: .medium)
+
+        /// A font for small plus body with size 13 and regular weight
+        static let bodySmallPlus = CKOUIStyle.font(size: 13, weight: .regular)
+
+        // MARK: - Input -
+
+        /// A font for input label with size 15 and regular weight
+        static let inputLabel = CKOUIStyle.font(size: 15, weight: .regular)
+
+        // MARK: - Action -
+
+        /// A font for large action with size 15 and medium weight
+        static let actionLarge = CKOUIStyle.font(size: 15, weight: .medium)
+
+        /// A font for default action with size 15 and regular weight
+        static let actinDefault = CKOUIStyle.font(size: 15, weight: .regular)
 
     }
 }
 
-private extension CKOUIStyle {
-     static func font(size: CGFloat, weight: UIFont.Weight) -> UIFont {
+extension CKOUIStyle {
+    /**
+        Return the common used font in Checkout UI
+
+        - Parameters:
+           - size: size of the font like 24
+           - weight: weight of the font like medium
+
+        - Returns: UIFont
+        */
+    static func font(size: CGFloat, weight: UIFont.Weight) -> UIFont {
         UIFont.systemFont(ofSize: size, weight: weight)
     }
 }
---
+++ Tests/UI/CKOUIStyle/CKOUIStyleFontTests.swift
@@ -0,0 +1,127 @@
+import XCTest
+@testable import Frames
+
+final class CKOUIStyleFontTests: XCTestCase {
+
+    // MARK: - Background -
+
+    func testBackgroundPrimaryColor(){
+        let color = CKOUIStyle.Color.backgroundPrimary
+        let expectedColor = UIColor(hex: "#FFFFFF")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    // MARK: - Text -
+
+    func testTextPrimaryColor(){
+        let color = CKOUIStyle.Color.textPrimary
+        let expectedColor = UIColor(hex: "#000000")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testTextSecondaryColor(){
+        let color = CKOUIStyle.Color.textSecondary
+        let expectedColor = UIColor(hex: "#727272")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+    
+    func testTextActionPrimaryColor(){
+        let color = CKOUIStyle.Color.textActionPrimary
+        let expectedColor = UIColor(hex: "#727272")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testTextActionSecondaryColor(){
+        let color = CKOUIStyle.Color.textActionSecondary
+        let expectedColor = UIColor(hex: "#0B5FF0")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testTextDisabledColor(){
+        let color = CKOUIStyle.Color.textDisabled
+        let expectedColor = UIColor(hex: "#727272")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testTextErrorColor(){
+        let color = CKOUIStyle.Color.textError
+        let expectedColor = UIColor(hex: "#AD283E")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    // MARK: - Border -
+
+    func testBorderPrimaryColor(){
+        let color = CKOUIStyle.Color.borderPrimary
+        let expectedColor = UIColor(hex: "#8A8A8A")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+    
+    func testBorderSecondaryColor(){
+        let color = CKOUIStyle.Color.borderSecondary
+        let expectedColor = UIColor(hex: "#D9D9D9")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testBorderActiveColor(){
+        let color = CKOUIStyle.Color.borderActive
+        let expectedColor = UIColor(hex: "#0B5FF0")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testBorderErrorColor(){
+        let color = CKOUIStyle.Color.borderError
+        let expectedColor = UIColor(hex: "#AD283E")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+    
+    // MARK: - Icon -
+
+    func testIconPrimaryColor(){
+        let color = CKOUIStyle.Color.iconPrimary
+        let expectedColor = UIColor(hex: "#000000")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testIconDisabledColor(){
+        let color = CKOUIStyle.Color.iconDisabled
+        let expectedColor = UIColor(hex: "#8A8A8A")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testIconActionColor(){
+        let color = CKOUIStyle.Color.iconAction
+        let expectedColor = UIColor(hex: "#0B5FF0")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    // MARK: - Action -
+
+    func testActionPrimaryColor(){
+        let color = CKOUIStyle.Color.actionPrimary
+        let expectedColor = UIColor(hex: "#0B5FF0")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+
+    func testActionDisabledColor(){
+        let color = CKOUIStyle.Color.actionDisabled
+        let expectedColor = UIColor(hex: "#F0F0F0")
+
+        XCTAssertEqual(color, expectedColor)
+    }
+}
