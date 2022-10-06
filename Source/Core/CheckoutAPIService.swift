94ca0a7d81456e3559b3675764b7d93aa4963838
Alex Ioja-Yang <alex.ioja-yang@checkout.com>
2022-10-06 11:14:39 +0100

    remove single time events & use hardcoded correlation id value

--- Source/Core/CheckoutAPIService.swift
+++ Source/Core/CheckoutAPIService.swift
@@ -25,7 +25,7 @@ public final class CheckoutAPIService: CheckoutAPIProtocol {
     public init(publicKey: String, environment: Environment) {
         let checkoutAPIService = Checkout.CheckoutAPIService(publicKey: publicKey, environment: environment.checkoutEnvironment)
         let cardValidator = CardValidator(environment: environment.checkoutEnvironment)
-        let logger = FramesEventLogger(environment: environment) { checkoutAPIService.correlationID }
+        let logger = FramesEventLogger(environment: environment, correlationID: checkoutAPIService.correlationID)
 
         self.checkoutAPIService = checkoutAPIService
         self.cardValidator = cardValidator
