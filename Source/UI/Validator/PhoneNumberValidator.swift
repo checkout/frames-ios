99f26d0c7f9728848e468b9f1d6e109c3789e88b
Ehab Al <ehab.alsharkawy@checkout.com>
2022-11-01 11:35:22 +0000

    Update PhoneNumber validation logic

--- Source/UI/Validator/PhoneNumberValidator.swift
+++ Source/UI/Validator/PhoneNumberValidator.swift
@@ -1,12 +1,13 @@
 import Foundation
 
 class PhoneNumberValidator: Validator {
-    func validate(text: String?) -> Bool {
-        isEmpty(text: text)
-    }
+    private let maxDigitLength = 25
+    private let minDigitLength = 6
 
-    private func isEmpty(text: String?) -> Bool {
-        text?.isEmpty ?? false
+    func validate(text: String?) -> Bool {
+        guard let text = text else { return true }
+        let number = text.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        return number.isEmpty || number.count < 6 || number.count > 25
     }
 
 }
