520ec3eea3cbea53b36350cf0b217c5d948a9c39
Ehab Al <ehab.alsharkawy@checkout.com>
2022-11-03 11:20:30 +0000

    Remove unused function

--- Source/Extensions/UIViewExtensions.swift
+++ Source/Extensions/UIViewExtensions.swift
@@ -2,34 +2,6 @@ import Foundation
 import UIKit
 
 extension UIView {
-
-    func addScrollViewContraints(scrollView: UIScrollView, contentView: UIView) -> NSLayoutConstraint {
-        // Content View Constraints
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-        contentView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1.0).isActive = true
-        let contentViewHeightConstraint = contentView.heightAnchor.constraint(equalTo: heightAnchor,
-                                                                              multiplier: 1.0)
-        contentViewHeightConstraint.priority = .defaultLow
-        contentViewHeightConstraint.isActive = true
-        // Scroll View Constraints
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        scrollView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
-
-        let scrollViewBottomConstraint = scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 20)
-        scrollViewBottomConstraint.isActive = true
-        // return scrollView bottom anchor constraint, used to manage the keyboard
-        return scrollViewBottomConstraint
-    }
-
-}
-
-extension UIView {
     func setupConstraintEqualTo(view: UIView, constant: CGFloat = 0) {
         NSLayoutConstraint.activate([
             topAnchor.constraint(equalTo: view.topAnchor, constant: constant),
