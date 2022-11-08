17581cd0d17a83e8d05ce3446eac0438481cdd01
Ehab Al <ehab.alsharkawy@checkout.com>
2022-11-08 14:16:25 +0000

    Update version number

--- Source/Core/Constants/Constants.swift
+++ Source/Core/Constants/Constants.swift
@@ -8,7 +8,7 @@
 enum Constants {
 
     static let productName = "frames-ios-sdk"
-    static let version = "4.0.0"
+    static let version = "4.0.1"
     static let userAgent = "checkout-sdk-frames-ios/\(version)"
 
     enum Logging {
