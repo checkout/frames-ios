93f02c7955c49f3f5c2f2a95d6b9262f91ffa294
Ehab Al <ehab.alsharkawy@checkout.com>
2022-11-14 08:04:23 +0000

    Add ElementBorderStyle protocol

---
+++ Source/UI/CommonUI/Protocols/Elements/ElementBorderStyle.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public protocol ElementBorderStyle {
+    var cornerRadius: CGFloat { get }
+    var thickness: CGFloat { get }
+    var normalColor: UIColor { get }
+    var focusColor: UIColor { get }
+    var errorColor: UIColor { get }
+    var edges: UIRectEdge { get }
+    var corners: UIRectCorner? { get }
+}
