2db30a49d663b93dcdce4ce1f6c16d29e561a2e5
Ehab Al <ehab.alsharkawy@checkout.com>
2022-11-21 08:03:59 +0000

    Update Elements protocol

--- Source/UI/CommonUI/Protocols/Elements/ElementBorderStyle.swift
+++ Source/UI/CommonUI/Protocols/Elements/ElementBorderStyle.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-public protocol ElementBorderStyle {
-    var cornerRadius: CGFloat { get }
-    var borderWidth: CGFloat { get }
-    var normalColor: UIColor { get }
-    var focusColor: UIColor { get }
-    var errorColor: UIColor { get }
-    var edges: UIRectEdge? { get }
-    var corners: UIRectCorner? { get }
-}
+    public protocol ElementBorderStyle {
+        var cornerRadius: CGFloat { get }
+        var borderWidth: CGFloat { get }
+        var normalColor: UIColor { get }
+        var focusColor: UIColor { get }
+        var errorColor: UIColor { get }
+        var edges: UIRectEdge? { get }
+        var corners: UIRectCorner? { get }
+    }
