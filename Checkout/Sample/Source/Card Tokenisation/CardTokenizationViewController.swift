2e21b3941bbaf2cbb93a1234feacdd1a1759f8d4
Alex Ioja-Yang <alex.ioja-yang@checkout.com>
2023-01-04 14:55:15 +0000

    Update API key in sample project

--- Checkout/Sample/Source/Card Tokenisation/CardTokenizationViewController.swift
+++ Checkout/Sample/Source/Card Tokenisation/CardTokenizationViewController.swift
@@ -122,7 +122,7 @@ class CardTokenizationViewController: UIViewController {
       phone: phone)
 
     let checkoutAPIService = CheckoutAPIService(
-      publicKey: "pk_test_6e40a700-d563-43cd-89d0-f9bb17d35e73",
+      publicKey: "pk_sbox_4zwxstydpp7tytwedivr75oiyie",
       environment: .sandbox)
     checkoutAPIService.createToken(.card(card)) { result in
       switch result {
