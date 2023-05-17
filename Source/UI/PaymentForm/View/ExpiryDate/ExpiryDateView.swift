ff135821c0d6d8485cf23f9ccc068bb933476083
Alex Ioja-Yang <alex.ioja-yang@checkout.com>
2023-05-17 14:27:50 +0100

    Integrate new formatter into the view

--- Source/UI/PaymentForm/View/ExpiryDate/ExpiryDateView.swift
+++ Source/UI/PaymentForm/View/ExpiryDate/ExpiryDateView.swift
@@ -9,13 +9,12 @@ protocol ExpiryDateViewDelegate: AnyObject {
   func update(result: Result<ExpiryDate, ExpiryDateError>)
 }
 
-public final class ExpiryDateView: UIView {
+final class ExpiryDateView: UIView {
+
   weak var delegate: ExpiryDateViewDelegate?
 
-  /// 5 is the expected text count, for example "11/35".
-  private let dateFormatTextCount = 5
   private(set) var style: CellTextFieldStyle?
-  private let cardValidator: CardValidator
+  private let dateFormatter: ExpiryDateFormatter
 
   private(set) lazy var dateInputView: InputView = {
     let view = InputView().disabledAutoresizingIntoConstraints()
@@ -24,8 +23,9 @@ public final class ExpiryDateView: UIView {
   }()
 
   init(cardValidator: CardValidator) {
-    self.cardValidator = cardValidator
+    self.dateFormatter = ExpiryDateFormatter(cardValidator: cardValidator)
     super.init(frame: .zero)
+
     // setup expiry DateView
     addSubview(dateInputView)
     dateInputView.setupConstraintEqualTo(view: self)
@@ -38,121 +38,15 @@ public final class ExpiryDateView: UIView {
   func update(style: CellTextFieldStyle?) {
     self.style = style
     self.style?.textfield.isSupportingNumericKeyboard = true
-    if validateInputChanges(of: self.style?.textfield.text ?? "", newInput: "") != nil {
-      self.style?.textfield.text = ""
-    }
     dateInputView.update(style: self.style)
   }
 
-  func validateInputChanges(of textfieldText: String, newInput: String) -> ValidationError.ExpiryDate? {
-    let date = textfieldText + newInput
-    guard date.count == dateFormatTextCount else { return .incompleteYear }
-
-    let subString = date.split(separator: "/")
-    guard let month = subString.first,
-          month.count == 2,
-          let monthDigit = Int(month),
-          let year = subString.last,
-          year.count == 2,
-          let yearDigit = Int(year) else {
-      return .invalidYearString
-    }
-
-    switch cardValidator.validate(expiryMonth: monthDigit, expiryYear: yearDigit) {
-      case .success:
-        let expiryDate = ExpiryDate(month: monthDigit, year: yearDigit)
-        delegate?.update(result: .success(expiryDate))
-        return nil
-      case .failure(let error):
-        return error
-    }
-  }
-
   // MARK: - private functions
-
-  /*
-   Input validation from location 0 to 4 (MM/yy):
-   ===============
-
-   if location 0 (first month digit)
-   if user enters a number between 2-9,
-   then move to the year selector and append a 0 before the digit
-
-   if location 1 (2nd month digit)
-   if user enters a 0,
-   then enter any digit between 1-9
-   if user enters a 1,
-   then enter any digit between 0-2
-
-   if location 3 (backslash '/')
-   then user must enter a number between 2-9 to start with
-
-   if location 4 (first year digit)
-   then user must enter a number between 2-9 to start with
-
-   if location 5 (2nd year digit)
-   then validate full Expiry Date
-   */
-
-  private func validateInput(_ textField: UITextField, location: Int, replacementText: String) -> Bool {
-    // check for max length including added spacers which all equal to 5
-    guard !replacementText.isEmpty else { return false }
-    let replacementText = replacementText.replacingOccurrences(of: " ", with: "")
-
-    // verify entered text is a numeric value
-    guard let currentDigit = Int(replacementText) else { return false }
-
-    switch location {
-      case 0 where 2...9 ~= currentDigit:
-        textField.text = "0"
-
-      case 1:
-        guard let originalText = textField.text, let previousDigit = Int(originalText) else {
-          updateErrorViewStyle(isHidden: false, textfieldText: textField.text, error: .invalidMonth)
-          return false
-        }
-
-        switch previousDigit {
-          case  0 where 1...9 ~= currentDigit,
-                1 where 0...2 ~= currentDigit:
-            break
-          default:
-            updateErrorViewStyle(isHidden: false, textfieldText: textField.text, error: .invalidMonth)
-            return false
-        }
-
-      case 2:
-        textField.text?.append("/")
-        guard 2...9 ~= currentDigit else {
-          updateErrorViewStyle(isHidden: false, textfieldText: textField.text, error: .invalidYear)
-          return false
-        }
-
-      case 3:
-        guard 2...9 ~= currentDigit else {
-          updateErrorViewStyle(isHidden: false, textfieldText: textField.text, error: .invalidYear)
-          return false
-        }
-
-      case 4:
-        let text = textField.text ?? ""
-        let error = validateInputChanges(of: text, newInput: replacementText)
-        updateErrorViewStyle(isHidden: error == nil, textfieldText: text, error: error)
-        return error == nil
-      default: break
-    }
-
-    return true
-  }
-
   private func updateErrorViewStyle(isHidden: Bool, textfieldText: String?, error: ValidationError.ExpiryDate? = nil) {
     style?.error?.text = error == .inThePast ?
       Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.past :
       Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid
     style?.error?.isHidden = isHidden
-    if !isHidden {
-      delegate?.update(result: .failure(.invalidCode))
-    }
     style?.textfield.text = textfieldText ?? ""
     dateInputView.update(style: style)
   }
@@ -162,9 +56,13 @@ extension ExpiryDateView: TextFieldViewDelegate {
   func textFieldShouldBeginEditing(textField: UITextField) {}
   func textFieldShouldReturn() -> Bool { return false }
   func textFieldShouldEndEditing(textField: UITextField, replacementString: String) -> Bool {
-    if textField.text?.count ?? 0 < dateFormatTextCount {
-      updateErrorViewStyle(isHidden: false, textfieldText: textField.text, error: .invalidMonth)
+    guard textField.text?.count ?? 0 != dateFormatter.dateFormatTextCount else {
+      return true
     }
+    let (displayValue, _) = dateFormatter.formatForDisplay(input: textField.text ?? "")
+    updateErrorViewStyle(isHidden: false,
+                         textfieldText: displayValue,
+                         error: .incompleteYear)
     return true
   }
 
@@ -173,29 +71,79 @@ extension ExpiryDateView: TextFieldViewDelegate {
   }
 
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    let outputText = textField.newTextAfter(applyingString: string, at: range) ?? ""
 
-    updateErrorViewStyle(isHidden: true, textfieldText: textField.text)
-    /*
-     Expiry date text format is "MM/yy"
-     5 is the expected text count, for example "11/35".
-     location starts from 0 to dateFormatTextCount - 1
-     */
-    guard range.location < dateFormatTextCount else { return false }
-
-    // Hide error view on remove from last location
-    if range.location == dateFormatTextCount - 1 {
-      updateErrorViewStyle(isHidden: true, textfieldText: textField.text)
+    defer {
+      delegate?.update(result: dateFormatter.createCardExpiry(from: outputText))
+    }
+    // If string is empty when this is called, it means there was no character added to the change
+    // meaning it is a deletion. We will not reformat a deletion as we risk to block user
+    // We will just allow TextField to do its lifecycle and record modified value
+    guard !string.isEmpty else {
+      style?.textfield.text = outputText
+        return true
     }
 
-    if range.length > 0 {
-      if range.location == dateFormatTextCount - 2 {
-        var originalText = textField.text
-        originalText = originalText?.replacingOccurrences(of: "/", with: "")
-        textField.text = originalText
-      }
-      return true
-    }
+    // When the input is changed we will reformat it for display, notifying its consumers
+    // of the new value
+    let (displayValue, error) = dateFormatter.formatForDisplay(input: outputText)
 
-    return validateInput(textField, location: range.location, replacementText: string)
+    // Receiving nil from the formatting means the new input is invalid and we should not accept it,
+    // dropping it completely
+    guard displayValue != nil else {
+      return false
+    }
+    updateErrorViewStyle(isHidden: error == nil,
+                         textfieldText: displayValue,
+                         error: error)
+    // As we notify consumers, we already update the input value, so we do not want
+    // the lifecycle to duplicate the update
+    return false
   }
 }
+
+/*
+ 
+ func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+     guard textField.text?.count ?? 0 != dateFormatter.dateFormatTextCount else {
+         return true
+     }
+     let (displayValue, _) = dateFormatter.formatForDisplay(input: textField.text ?? "")
+     updateErrorViewStyle(isHidden: false,
+                          textfieldText: displayValue,
+                          error: .incompleteYear)
+     return true
+ }
+
+ func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+     let outputText = textField.newTextAfter(applyingString: string, at: range) ?? ""
+
+     defer {
+         delegate?.update(result: dateFormatter.createCardExpiry(from: outputText))
+     }
+     // If string is empty when this is called, it means there was no character added to the change
+     // meaning it is a deletion. We will not reformat a deletion as we risk to block user
+     // We will just allow TextField to do its lifecycle and record modified value
+     guard !string.isEmpty else {
+         style?.textfield.text = outputText
+         return true
+     }
+
+     // When the input is changed we will reformat it for display, notifying its consumers
+     // of the new value
+     let (displayValue, error) = dateFormatter.formatForDisplay(input: outputText)
+
+     // Receiving nil from the formatting means the new input is invalid and we should not accept it,
+     // dropping it completely
+     guard displayValue != nil else {
+         return false
+     }
+     updateErrorViewStyle(isHidden: error == nil,
+                          textfieldText: displayValue,
+                          error: error)
+     // As we notify consumers, we already update the input value, so we do not want
+     // the lifecycle to duplicate the update
+     return false
+ }
+
+ */
--- Tests/UI/BillingForm/View/ExpiryDateViewTests.swift
+++ Tests/UI/BillingForm/View/ExpiryDateViewTests.swift
@@ -4,253 +4,85 @@ import Checkout
 
 class ExpiryDateViewTests: XCTestCase {
 
-  var view: ExpiryDateView!
-  var style: DefaultExpiryDateFormStyle!
-
-  override func setUp() {
-    super.setUp()
-    style = DefaultExpiryDateFormStyle()
-    view = ExpiryDateView(cardValidator: CardValidator(environment: .sandbox))
-    view.update(style: style)
-  }
-
-  // Invalid string case of of pre-filled expiry date text from the merchant.
-  func testInValidStringPrefilledTextFieldTextStyle(){
-    style.textfield.text = "Test"
-    view.update(style: style)
-
-    XCTAssertEqual(view.dateInputView.textFieldView.textField.text, "")
-  }
-
-  // Invalid old date case of of pre-filled expiry date text from the merchant.
-  func testInValidOldDatePrefilledTextFieldTextStyle(){
-    style.textfield.text = "10/19"
-    view.update(style: style)
-
-    XCTAssertEqual(view.dateInputView.textFieldView.textField.text, "")
-  }
-
   // Valid date case of pre-filled expiry date text from the merchant.
-  func testValidDatePrefilledTextFieldTextStyle(){
-    style.textfield.text = "10/77"
-    view.update(style: style)
+    func testValidDatePrefilledTextFieldTextStyle() {
+        var style = DefaultExpiryDateFormStyle()
+        style.textfield.text = "10/77"
+        let view = ExpiryDateView(cardValidator: CardValidator(environment: .sandbox))
+        view.update(style: style)
+        view.update(style: style)
+        
+        XCTAssertEqual(view.dateInputView.textFieldView.textField.text, "10/77")
+    }
+    
+    func testValidExpiryDate() {
+        let view = createView()
+        let mockDelegate = MockExpiryDateViewDelegate()
+        view.delegate = mockDelegate
+        let textField = UITextField()
+        textField.text = "01/7"
+        XCTAssertFalse(view.textField(textField, shouldChangeCharactersIn: NSRange(location: 4, length: 0), replacementString: "0"))
+        XCTAssertTrue(view.style?.error?.isHidden == true)
+        XCTAssertEqual(view.style?.textfield.text, "01/70")
+        
+        XCTAssertEqual(mockDelegate.receivedResults.count, 1)
+        switch mockDelegate.receivedResults.first {
+        case .success(let expiry):
+            XCTAssertEqual(expiry.month, 1)
+            XCTAssertEqual(expiry.year, 2070)
+        default:
+            XCTFail("Should call delegate with a success!")
+        }
+    }
 
-    XCTAssertEqual(view.dateInputView.textFieldView.textField.text, "10/77")
-  }
+    func testValidTodayExpiryDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/yy"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let dateText = dateFormatter.string(from: Date())
+        let view = createView()
+        let mockDelegate = MockExpiryDateViewDelegate()
+        view.delegate = mockDelegate
+        let textField = UITextField()
+        textField.text = dateText
+        XCTAssertTrue(view.textField(textField, shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: ""))
+        XCTAssertTrue(view.style?.error?.isHidden == true)
+        XCTAssertEqual(view.style?.textfield.text, dateText)
+        
+        XCTAssertEqual(mockDelegate.receivedResults.count, 1)
+        switch mockDelegate.receivedResults.first {
+        case .success(let expiry):
+            XCTAssertEqual(expiry.month, Int(dateText.prefix(2)))
+            XCTAssertEqual(expiry.year, Int("20\(dateText.suffix(2))"))
+        default:
+            XCTFail("Should call delegate with a success!")
+        }
+    }
 
-  func testValidExpiryDate(){
-    let error = view.validateInputChanges(of: "01/7", newInput: "0")
-    XCTAssertNil(error)
-    XCTAssertTrue(view.style?.error?.isHidden ?? false)
-  }
-
-  func testValidTodayExpiryDate(){
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "MM/yy"
-    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    let dateText = dateFormatter.string(from: Date())
-    let error = view.validateInputChanges(of: dateText, newInput: "")
-    XCTAssertNil(error)
-    XCTAssertTrue(view.style?.error?.isHidden ?? false)
-  }
-
-  func testPastExpiryDate(){
-    let error = view.validateInputChanges(of: "01/1", newInput: "2")
-    XCTAssertEqual(error, .inThePast)
-  }
-
-  func testEmptyExpiryDate(){
-    let error = view.validateInputChanges(of: "", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithWrongYearFormat(){
-    let error = view.validateInputChanges(of: "01/2035", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithWrongMonthFormat(){
-    let error = view.validateInputChanges(of: "Jan/35", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-
-  func testExpiryDateWithWrongFormat(){
-    let error = view.validateInputChanges(of: "01.35", newInput: "")
-    XCTAssertEqual(error, .invalidYearString)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithMoreThan5Characters(){
-    let error = view.validateInputChanges(of: "01/01/01/01", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithOutBackSlash(){
-    let error = view.validateInputChanges(of: "01350", newInput: "")
-    XCTAssertEqual(error, .invalidYearString)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithmoreLess5Characters(){
-    let error = view.validateInputChanges(of: "01/0", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithString(){
-    let error = view.validateInputChanges(of: "Test", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithInvalidMaxDate(){
-    let error = view.validateInputChanges(of: "99/99", newInput: "")
-    XCTAssertEqual(error, .invalidMonth)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithInvalidMinDate(){
-    let error = view.validateInputChanges(of: "00/00", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithInvalidLongNumbers(){
-    let error = view.validateInputChanges(of: "999999999/999999999", newInput: "")
-    XCTAssertEqual(error, .incompleteYear)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testExpiryDateWithInvalidLongSpecialCharacter(){
-    let error = view.validateInputChanges(of: "-*/@@", newInput: "")
-    XCTAssertEqual(error, .invalidYearString)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-  }
-
-  func testValidFirstDigitInputWith0() {
-    let textField = UITextField()
-    textField.text = ""
-    let input = "0"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: input)
-    XCTAssertTrue(shouldContinueAdding)
-
-    textField.text?.append(input)
-    XCTAssertEqual(textField.text, "0")
-  }
-
-  func testInvalidSecondDigitInputWith0() {
-    let textField = UITextField()
-    textField.text = "0"
-    let input = "0"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 1, length: 0), replacementString: input)
-
-    XCTAssertFalse(shouldContinueAdding)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-    XCTAssertEqual(textField.text, "0")
-  }
-
-  func testValidSecondDigitInputWith3() {
-    let textField = UITextField()
-    textField.text = ""
-    let input = "3"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: input)
-    XCTAssertTrue(shouldContinueAdding)
-
-    textField.text?.append(input)
-    XCTAssertEqual(textField.text, "03")
-  }
-
-  func testValidSecondDigitInputWith2() {
-    let textField = UITextField()
-    textField.text = "1"
-    let input = "2"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 1, length: 0), replacementString: input)
-    XCTAssertTrue(shouldContinueAdding)
-
-    textField.text?.append(input)
-    XCTAssertEqual(textField.text, "12")
-  }
-
-  func testInvalidSecondDigitInputWith9() {
-    let textField = UITextField()
-    textField.text = "1"
-    let input = "9"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 1, length: 0), replacementString: input)
-
-    XCTAssertFalse(shouldContinueAdding)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-    XCTAssertEqual(textField.text, "1")
-  }
-
-  func testInvalidthirdDigitInputWith0() {
-    let textField = UITextField()
-    textField.text = "01"
-    let input = "0"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 2, length: 0), replacementString: input)
-
-    XCTAssertFalse(shouldContinueAdding)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-    XCTAssertEqual(textField.text, "01/")
-  }
-
-  func testInvalidthirdDigitInputWith1() {
-    let textField = UITextField()
-    textField.text = "02"
-    let input = "1"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 2, length: 0), replacementString: input)
-
-    XCTAssertFalse(shouldContinueAdding)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-    XCTAssertEqual(textField.text, "02/")
-  }
-
-  func testValidthirdDigitInputWith2() {
-    let textField = UITextField()
-    textField.text = "02"
-    let input = "2"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 2, length: 0), replacementString: input)
-    XCTAssertTrue(shouldContinueAdding)
-
-    textField.text?.append(input)
-    XCTAssertEqual(textField.text, "02/2")
-  }
-
-  func testValidFourthDigitInputWith2() {
-    let textField = UITextField()
-    textField.text = "02/3"
-    let input = "2"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 3, length: 0), replacementString: input)
-    XCTAssertTrue(shouldContinueAdding)
-
-    textField.text?.append(input)
-    XCTAssertEqual(textField.text, "02/32")
-  }
-
-  func testInvalidFourthDigitInputWith() {
-    let textField = UITextField()
-    textField.text = "02/2"
-    let input = "0"
-
-    let shouldContinueAdding = view.textField(textField, shouldChangeCharactersIn: NSRange(location: 3, length: 0), replacementString: input)
-
-    XCTAssertFalse(shouldContinueAdding)
-    XCTAssertEqual(view.style?.error?.text, Constants.LocalizationKeys.PaymentForm.ExpiryDate.Error.invalid)
-    XCTAssertEqual(textField.text, "02/2")
-  }
-
+    func testPastExpiryDate() {
+        let view = createView()
+        let mockDelegate = MockExpiryDateViewDelegate()
+        view.delegate = mockDelegate
+        let textField = UITextField()
+        textField.text = "01/1"
+        XCTAssertFalse(view.textField(textField, shouldChangeCharactersIn: NSRange(location: 4, length: 0), replacementString: "2"))
+        XCTAssertTrue(view.style?.error?.isHidden == false)
+        XCTAssertEqual(view.style?.textfield.text, "01/12")
+        
+        XCTAssertEqual(mockDelegate.receivedResults.count, 1)
+        switch mockDelegate.receivedResults.first {
+        case .failure(let error):
+            XCTAssertEqual(error, .invalidCode)
+        default:
+            XCTFail("Should call delegate with a failure!")
+        }
+    }
+    
+    private func createView(validator: CardValidating = CardValidator(environment: .sandbox),
+                            style: CellTextFieldStyle = DefaultExpiryDateFormStyle()) -> ExpiryDateView {
+        let view = ExpiryDateView(cardValidator: CardValidator(environment: .sandbox))
+        view.update(style: style)
+        return view
+    }
 }
 
