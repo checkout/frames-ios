9494332acd1b986a1ff8128ccc2344cc82c33a00
Alex Ioja-Yang <alex.ioja-yang@checkout.com>
2023-06-13 10:25:21 +0100

    Reintroduce properties as deprecated and provide default implementations

--- Source/UI/CommonUI/Protocols/Elements/ElementTextFieldStyle.swift
+++ Source/UI/CommonUI/Protocols/Elements/ElementTextFieldStyle.swift
@@ -6,4 +6,32 @@ public protocol ElementTextFieldStyle: ElementStyle {
     var placeholder: String? { get }
     var tintColor: UIColor { get }
     var borderStyle: ElementBorderStyle { get }
+
+    @available(*, deprecated, message: "Property will be removed soon. Use borderStyle.cornerRadius instead")
+    var cornerRadius: CGFloat { get }
+
+    @available(*, deprecated, message: "Property will be removed soon. Use borderStyle.borderWidth instead")
+    var borderWidth: CGFloat { get }
+}
+
+public extension ElementTextFieldStyle {
+
+    var cornerRadius: CGFloat {
+        Constants.Style.BorderStyle.cornerRadius
+    }
+
+    var borderWidth: CGFloat {
+        Constants.Style.BorderStyle.borderWidth
+    }
+
+    var borderStyle: ElementBorderStyle {
+        // Deprecated warning required to encourage migrating away from using these properties
+        DefaultBorderStyle(cornerRadius: cornerRadius,
+                           borderWidth: borderWidth,
+                           normalColor: FramesUIStyle.Color.borderPrimary,
+                           focusColor: FramesUIStyle.Color.borderActive,
+                           errorColor: FramesUIStyle.Color.borderError,
+                           edges: .all,
+                           corners: .allCorners)
+    }
 }
--- Source/UI/PaymentForm/StyleProtocol/BillingSummaryViewStyle.swift
+++ Source/UI/PaymentForm/StyleProtocol/BillingSummaryViewStyle.swift
@@ -2,6 +2,34 @@ import UIKit
 
 public protocol BillingSummaryViewStyle: CellButtonStyle {
     var summary: ElementStyle? { get set }
-    var borderStyle: ElementBorderStyle { get set }
+    var borderStyle: ElementBorderStyle { get }
     var separatorLineColor: UIColor { get set }
+
+    @available(*, deprecated, message: "Property will be removed soon. Use borderStyle.cornerRadius instead")
+    var cornerRadius: CGFloat { get }
+
+    @available(*, deprecated, message: "Property will be removed soon. Use borderStyle.borderWidth instead")
+    var borderWidth: CGFloat { get }
+}
+
+public extension BillingSummaryViewStyle {
+
+    var cornerRadius: CGFloat {
+        Constants.Style.BorderStyle.cornerRadius
+    }
+
+    var borderWidth: CGFloat {
+        Constants.Style.BorderStyle.borderWidth
+    }
+
+    var borderStyle: ElementBorderStyle {
+        // Deprecated warning required to encourage migrating away from using these properties
+        DefaultBorderStyle(cornerRadius: cornerRadius,
+                           borderWidth: borderWidth,
+                           normalColor: FramesUIStyle.Color.borderPrimary,
+                           focusColor: FramesUIStyle.Color.borderActive,
+                           errorColor: FramesUIStyle.Color.borderError,
+                           edges: .all,
+                           corners: .allCorners)
+    }
 }
