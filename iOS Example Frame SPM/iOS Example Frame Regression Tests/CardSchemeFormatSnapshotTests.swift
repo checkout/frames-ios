31f83e5d9fe02fba69f51e001f6fbbe85c42978f
Okhan Okbay <okhan.okbay@checkout.com>
2023-08-03 21:07:40 +0100

    Add card scheme format snapshot tests

---
+++ iOS Example Frame SPM/iOS Example Frame Regression Tests/CardSchemeFormatSnapshotTests.swift
@@ -0,0 +1,27 @@
+//
+//  CardSchemeFormatSnapshotTests.swift
+//  iOS Example Frame Regression Tests
+//
+//  Created by Okhan Okbay on 03/08/2023.
+//  Copyright © 2023 Checkout. All rights reserved.
+//
+
+import Frames
+import SnapshotTesting
+import XCTest
+
+final class CardSchemeFormatSnapshotTests: XCTestCase {
+    
+    func testCardFormatting() {
+        tokenableTestCards.forEach { card in
+            let app = XCUIApplication()
+            app.launchFrames()
+            
+            let cardNumberTextField = app.otherElements[AccessibilityIdentifiers.PaymentForm.cardNumber]
+            app.enterText(card.number, into: cardNumberTextField)
+            app.tapDoneButton()
+            
+            assertSnapshot(matching: cardNumberTextField.screenshot().image, as: .image)
+        }
+    }
+}
