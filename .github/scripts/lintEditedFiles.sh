7cadfa74d94d6a9ece2c41544867503088b45fd6
Okhan Okbay <125963311+okhan-okbay-cko@users.noreply.github.com>
2023-09-19 11:47:14 +0100

    Add lint edited files script (#475)

---
+++ .github/scripts/lintEditedFiles.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -o pipefail
+git fetch origin main
+echo "Fetched"
+
+# Use a conditional to check if there are edited files
+if EDITED_FILES=$(git diff HEAD origin/main --name-only --diff-filter=d | grep "\.swift" | grep -v "\.swiftlint\.yml" | xargs echo | tr ' ' ','); then
+  echo "Got edited files"
+  echo $EDITED_FILES
+
+  # Check if EDITED_FILES is empty or null
+  if [ -z "$EDITED_FILES" ]; then
+    echo "No edited .swift files found."
+  else
+    swiftlint lint $EDITED_FILES | sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 title=Lint error,file=\1,line=\2,col=\3::\5\n\1:\2:\3/'
+  fi
+else
+  echo "No changes in .swift files found."
+fi
